b4e6297ecdf6c143705398b53baf3bc57635edeb
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2023-07-26 22:14:49 -0300

    chore: update sample to use qbone_options attr

--- sample_worker.rb
+++ sample_worker.rb
@@ -1,23 +1,23 @@
-$LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
+# frozen_string_literal: true
 
-require 'qbone'
+$LOAD_PATH.unshift File.expand_path("lib", __dir__)
+
+require "qbone"
 
 Qbone.configure do |config|
-#   config.redis = Redis.new
-#   config.logger = Logger.new(STDOUT)
+  #   config.redis = Redis.new
+  #   config.logger = Logger.new(STDOUT)
   config.logger.level = Logger::DEBUG
 end
 
 class SampleWorker
   include Qbone::Worker
 
-  QUEUE = 'sample'
-  RETRY = 3
+  qbone_options queue: "sample", retries: 3
 
   def perform(arg1, arg2)
     puts "Hello, world #{arg1} #{arg2}!"
   end
 end
 
-SampleWorker.perform_async('foo', 'bar')
-
+SampleWorker.perform_async("foo", "bar")
